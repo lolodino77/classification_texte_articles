0ca302c4138780bf1e8904a1ddeb98dbf96ab728
lolodino77 <laurentdangvu@gmail.com>
2023-01-26 22:59:30 +0100

    doc blogspot.py ok

--- sources/create_dataset/blogspot.py
+++ sources/create_dataset/blogspot.py
@@ -2,13 +2,16 @@ from blog import *
 
 
 class Blogspot(Blog):
+    """ Represente un blog Blogspot """
 
     def __init__(self, url, num_articles):
+        """ Constructeur de la classe Blogspot """
         Blog.__init__(self, url, num_articles)
         self.articles_urls = self.create_articles_urls()
         
 
     def __str__(self):
+        """ Descripteur de la classe Blogspot """
         desc = Blog.__str__(self)
         str_articles_urls = str(self.articles_urls)
         desc += "\narticles_urls = " + str_articles_urls
@@ -16,19 +19,30 @@ class Blogspot(Blog):
         return(desc)
 
 
-    def get_sitemap_from_main_sitemap(self, sitemap_page):
-        """ Donne la liste des urls sitemap presents sur la principale page sitemap d'un blog """
-        urls = self.get_web_page_text_contents(sitemap_page)
+    def get_sitemap_from_main_sitemap(self, main_sitemap_page):
+        """ Donne la liste des urls sitemap presents sur la principale page sitemap d'un blog 
+        Entree :
+            main_sitemap_page (string) : L'url de la page sitemap
+        Sortie :
+            urls (liste de string) : La liste des urls des pages sitemap
+        """
+        urls = self.get_web_page_text_contents(main_sitemap_page)
         urls = urls.replace('<?xml version=\'1.0\' encoding=\'UTF-8\'?><sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><sitemap>', "")
         urls = urls.replace("</loc></sitemap><sitemap>", "\n")
         urls = urls.replace("</loc></sitemap></sitemapindex>", "")
         urls = urls.replace("<loc>", "")
         urls = urls.split("\n")
+
         return(urls)
 
 
     def get_urls_from_one_sitemap_subpage(self, sitemap_subpage):
-        """ Recupere les urls sur une seule sous-page sitemap """
+        """ Recupere les urls sur une seule sous-page sitemap 
+        Entree :
+            sitemap_subpage (string) : Une sous-page sitemap
+        Sortie :
+            urls (liste de string) : Les urls sur une seule sous-page sitemap 
+        """
         print("in get_urls_from_one_sitemap_subpage")
         urls = self.get_web_page_text_contents(sitemap_subpage)
         urls = urls.replace("""<?xml version='1.0' encoding='UTF-8'?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">""", "")
@@ -52,9 +66,12 @@ class Blogspot(Blog):
 
 
     def create_articles_urls(self):
-        """ Renvoie dans une liste tous les articles d'un blog (wordpress ou blogspot) a partir de sa page d'accueil
+        """ Renvoie dans une liste des articles du blog a partir de sa page d'accueil
             Exemple : "https://majestyofreason.wordpress.com/", "https://edwardfeser.blogspot.com"
             Pour l'instant fonctionne que avec les blogs blogspot
+        
+        Sortie:
+            urls (liste de string) : Une liste d'urls + (ecriture du resultat dans le fichier filename)
         """
         # Recupere dans une liste urls les adresses url de tous les articles publies d'un blog
         sitemap_page = self.get_sitemap_page()
