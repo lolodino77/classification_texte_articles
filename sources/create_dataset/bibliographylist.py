67cb849b77f37f57826c782a7e214e1891182db3
lolodino77 <laurentdangvu@gmail.com>
2023-01-27 15:53:57 +0100

    doc bibliographylist.py bloglist.py ok

--- sources/create_dataset/bibliographylist.py
+++ sources/create_dataset/bibliographylist.py
@@ -3,31 +3,35 @@ from bibliography import *
 
 class BibliographyList(DataSourceList):
     def __init__(self, filename, num_articles, table_format):
-        """ Constructor of Bibliographylist """
+        """ Constructeur de la classe Bibliographylist """
         DataSourceList.__init__(self, filename, num_articles, table_format)
-        # self.dataSources = self.define_datasources()
         self.urls = self.define_urls()
         self.corpus_name = self.define_corpus_name()
         self.define_datasources()
         
     def define_corpus_name(self):
+        """ Definit l'attribut corpus_name (le nom du corpus, ici de la bibliographie) """
         corpus_name = self.filename.split("bibliography_")[1].split(".txt")[0]
         return(corpus_name)
 
     def define_urls(self):
-        """ Definit urls, si besoin va chercher la liste urls dans un fichier texte """
+        """ Definit l'attribut urls (une liste d'urls de blogs) a partir d'un fichier .txt qui les contient 
+        d'urls dans un fichier texte """
         # if(type(urls) == str and "http://" not in urls): #si urls = nom d'un fichier texte
         path = "./data/input/bibliographies/{}".format(self.filename)
         urls = open(path, "r", encoding="utf-8").read().split("\n")
         return(urls)
 
     def define_datasources(self):
-        """ Definit la liste d'objets DataSources (liste de Bibliography ou de Blog """
+        """ Definit l'attribut datasources (la liste d'objets DataSource), ici une liste d'objets Bibliography
+        """
         for url in self.urls:
             bibliography = Bibliography(url, self.corpus_name, self.num_articles)
             self.dataSources.append(bibliography)
 
     def save_articles_urls(self):
+        """ Pour chaque bibliographie, ecrit l'attribut articles_urls (tous les articles d'une bibliographie)
+        dans un fichier .txt different """
         bibliography = self.dataSources[0]
         bibliography.save_articles_urls(file_open_mode="w", sep = "\n")
         for bibliography in self.dataSources[1:]:
--- sources/create_dataset/bloglist.py
+++ sources/create_dataset/bloglist.py
@@ -6,22 +6,25 @@ from wordpress import *
 
 class BlogList(DataSourceList):
     def __init__(self, filename, num_articles, table_format):
-        """ Constructor of Bloglist """
+        """ Constructeur de la classe Bloglist """
         DataSourceList.__init__(self, filename, num_articles, table_format)
         self.urls = self.define_urls()
         self.define_datasources()
 
     def __str__(self):
+        """ Descripteur de la classe Bloglist """
         desc = DataSourceList.__str__(self)
         return(desc)
 
     def define_urls(self):
+        """ Definit l'attribut urls (une liste d'urls de blogs) a partir d'un fichier .txt qui les contient 
+        d'urls dans un fichier texte """        
         path = "./data/input/blogs/{}".format(self.filename)
         urls = open(path, "r", encoding="utf-8").read().split("\n")
         return(urls)
 
     def define_datasources(self):
-        """ Definit la liste d'objets DataSources (liste de Bibliography ou de Blog """
+        """ Definit l'attribut datasources (la liste d'objets DataSources), ici une liste d'objets Blog """
         for url in self.urls:
             if("blogspot" in url):
                 print("c'est blogspot")
@@ -34,5 +37,7 @@ class BlogList(DataSourceList):
             print("self.dataSources =", self.dataSources)
 
     def save_articles_urls(self):
+        """ Pour chaque blog, ecrit l'attribut articles_urls (des articles d'un blog, tous ou un nombre defini)
+        dans un fichier .txt different """
         for blog in self.dataSources:
             blog.save_articles_urls()
